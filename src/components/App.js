4184d9b56b9f7e1d099d040efa788ec018cb3a6e
davedwight <dpdwight@gmail.com>
2021-05-21 17:09:27 -0600

    Set up form state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Link } from "react-router-dom";
-import '../styles/App.css'
-import Home from './Home'
-import Form from './Form'
+import '../styles/App.css';
+import Home from './Home';
+import Form from './Form';
+import * as yup from 'yup';
+import schema from '../Validation/formSchema';
+import axios from 'axios';
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  pepperoni: false,
+  mushrooms: false,
+  olives: false,
+  sausage: false,
+  peppers: false,
+  special: '',
+};
+
+const initialFormErrors = {
+  name: ''
+};
+
+const initialOrders = [];
+
+const initialDisabled = false;
 
 const App = () => {
+
+  const [orders, setOrders] = useState(initialOrders);
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const postNewOrder = newOrder => {
+    axios.post('https://reqres.in/api/order', newOrder)
+      .then(res => {
+        setOrders([...orders, res.data])
+        console.log('Post response: ', res.data)
+      })
+      .catch(err => {
+        console.log('Error: ', err)
+      })
+      .finally(setFormValues(initialFormValues))
+  }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues, [name]: value
+    })
+  }
+  console.log('Form Values: ', formValues)
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      pepperoni: formValues.pepperoni,
+      mushrooms: formValues.mushrooms,
+      olives: formValues.olives,
+      sausage: formValues.sausage,
+      peppers: formValues.peppers,
+      special: formValues.special.trim(),
+    }
+    postNewOrder(newOrder)
+  }
+  console.log('Orders: ', orders);
+
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
+
   return (
     <div className='App'>
       
@@ -20,7 +94,7 @@ const App = () => {
       {/* End nav bar   */}
 
       {/* Home page */}
-      <Route path='/' component={Home} />
+      <Route exact path='/' component={Home} />
       {/* End home page */}
 
       {/* Order form */}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,116 @@
 import React from 'react';
+import '../styles/Form.css'
 
 export default function Form(props) {
-    return <div>Form component</div>
+
+    const { values, change, disabled, submit, errors } = props;
+
+    const onChange = event => {
+        const { name, value, checked, type } = event.target;
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, valueToUse);
+    }
+
+    const onSubmit = event => {
+        event.preventDefault();
+        submit();
+    }
+
+    return (
+        <div id="form-container" onSubmit={onSubmit}>
+
+            <form id='pizza-form'>
+
+            {/* Name input */}
+            <label>
+                <h4>{`Name `}</h4>
+                <input
+                    type='text'
+                    name='name'
+                    id='name-input'
+                    value={values.name}
+                    onChange={onChange}
+                />
+            </label>
+
+            {/* Pizza size dropdown */}
+            <label>
+                <h4>{`Pizza Size `}</h4>
+                <select id='size-dropdown' name='size' value={values.size} onChange={onChange}>
+                    <option value=''>-- Select Size -- </option>
+                    <option value='12'>12"</option>
+                    <option value='16'>16"</option>
+                    <option value='20'>20"</option>
+                </select>
+            </label>
+
+            {/* Toppings checklist */}
+            <h4>Choose toppings</h4>
+            <div id="checklist-container">
+                <label>
+                    <p>Pepperoni</p>
+                    <input 
+                        name='pepperoni'
+                        type='checkbox'
+                        value={values.pepperoni}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>
+                    <p>Mushrooms</p>
+                    <input 
+                        name='mushrooms'
+                        type='checkbox'
+                        value={values.mushrooms}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>
+                    <p>Olives</p>
+                    <input 
+                        name='olives'
+                        type='checkbox'
+                        value={values.olives}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>
+                    <p>Sausage</p>
+                    <input 
+                        name='sausage'
+                        type='checkbox'
+                        value={values.sausage}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>
+                    <p>Peppers</p>
+                    <input 
+                        name='peppers'
+                        type='checkbox'
+                        value={values.peppers}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+
+            {/* Special instructions input */}
+            <label>
+                <h4>{`Special Instructions `}</h4>
+                <input 
+                    type='text'
+                    name='special'
+                    id='special-text'
+                />
+            </label>
+
+            </form>
+            
+            <button disabled={disabled} id='order-button'>Add to Order</button>
+            
+            <div id='name-errors'>{errors.name}</div>
+
+        </div>
+        
+    )
 };

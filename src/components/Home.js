3547db8ed3fca9236c7aa3da86d39d9218959771
davedwight <dpdwight@gmail.com>
2021-05-21 14:53:42 -0600

    Styled home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import '../styles/Home.css';
 
 export default function Home(props) {
     return (
     <div>
-        <Link to='/pizza'>
-            <button>Order Pizza</button>
-        </Link>
+        <div id='img-container'>
+            <Link to='/pizza'>
+                <button>Order Pizza</button>
+            </Link>
+        </div>
     </div>
     )
 };
